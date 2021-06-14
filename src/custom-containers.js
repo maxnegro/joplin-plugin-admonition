39544a7bd506d1362e1e3aab7835c5641d3ea25e
Massimiliano Masserelli <mmasserelli@gmail.com>
2021-06-14 17:45:21 +0200

    Test markdown-it-container plugin

---
+++ src/custom-containers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    default: function(context) {
+        return {
+            plugin: require('markdown-it-container');
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
+		console.info('Hello world. Custom containers plugin started!');
+		// Here we register new Markdown plugin
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			'Custom containers',
+			'./custom-containers.js'
+		);
 	},
 });
