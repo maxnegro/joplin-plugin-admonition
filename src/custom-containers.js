2c0529fe794dde9c6bf32c48576b7a32c0fd6c46
Massimiliano Masserelli <mmasserelli@gmail.com>
2021-06-14 17:54:24 +0200

    Solved a couple of syntax errors

--- src/custom-containers.js
+++ src/custom-containers.js
@@ -1,7 +1,7 @@
 module.exports = {
     default: function(context) {
         return {
-            plugin: require('markdown-it-container');
+            plugin: require('markdown-it-container'),
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import joplin from 'api';
+import { ContentScriptType } from 'api/types';
+
 
 joplin.plugins.register({
 	onStart: async function() {
